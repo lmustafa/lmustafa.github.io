5efd6719a5aea0bdf4bf2ac4c4b5f51417a0fe1f
Laiba Mustafa <lmustafa@uoguelph.ca>
2020-06-23 00:03:23 -0400

    moving out

--- public/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    // Get value on button click and show alert
-    $("#myBtn").click(function(){
-        let str = $("#myInput").val();
-        alert(str);
-    });
-});
